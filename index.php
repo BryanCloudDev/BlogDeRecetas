8b86dae83f5cddf6afc6e122c43db393df3d8939
Bryan Portillo <bportillo701@gmail.com>
2022-03-17 15:18:09 -0600

    Agg gitignore y creando estructura header y footer

--- index.php
+++ index.php
@@ -19,19 +19,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
-//AQUI AGREGAREMOS EL HEADER
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Bienvenido 🥳</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
-<body>
+<?php require('./resources/header');?>
+
 <h1 class="my-5">Holaaaaaa, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenido a nuestro Blog!.</h1>
 
 </body>
