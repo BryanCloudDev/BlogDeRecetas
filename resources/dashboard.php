7ae500012f5e3c432f7fa6a3e35219e9503912fb
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-03-21 18:00:58 -0600

    Create dashboard.php
    Dashbord al 10%

---
+++ resources/dashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Admin dashboard</title>
+  </head>
+  <body>
+    <header style="background-color:red;"> 
+    <!-- La siguiente l[inea necesita mejorarse o completarse -->
+      <img id="" src="" width="100px" height="100px"/>
+      <h1>Admin dashboard</h1>
+        <p>Admin dashboard</p>
+    </header>
+  </body>
+</html>
+
+<?PHP
+/*
+usuarios()
+eliminarUsuario()
+posts()
+eliminarPosts()
+eliminarComentario()
+crearPost()
+comentarPost()
+
+header: string
+footer: string
+username: string
+usuarios: string
+logout: bool
+*/
+
+
+?>
+  
