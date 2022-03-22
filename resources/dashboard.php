f518ff54472beecbb5f52c0a61522c6316a1a481
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-03-21 21:34:18 -0600

    Update dashboard.php
    Mejoras

--- resources/dashboard.php
+++ resources/dashboard.php
@@ -5,11 +5,28 @@
   </head>
   <body>
     <header style="background-color:red;"> 
-    <!-- La siguiente l[inea necesita mejorarse o completarse -->
+    <!-- La siguiente linea necesita mejorarse y/o completarse -->
       <img id="" src="" width="100px" height="100px"/>
       <h1>Admin dashboard</h1>
         <p>Admin dashboard</p>
     </header>
+    <div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+ 
+        <div class="navbar nav_title" style="border: 0;">
+            <a href="1.html" class="site_title">Voy a dejar esto por aquí por si me sirve después</a>
+        </div>
+ 
+        <div class="profile"><!--img_2 -->
+            <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle pro_img">
+            </div>
+            <div class="profile_info">
+                <span>Admin area</span>
+                <h2>Admin</h2>
+            </div>
+        </div>
+ 
   </body>
 </html>
 
