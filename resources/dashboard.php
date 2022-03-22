57f672fa31413282f4f25a801ffc8c7089207c2e
Alejandroq12 <quezada.rodriguez6@gmail.com>
2022-03-21 21:44:20 -0600

    Update dashboard.php
    Modificación 5

--- resources/dashboard.php
+++ resources/dashboard.php
@@ -43,7 +43,65 @@
                             <li><a href="3.html">modificar luego</a></li>
                         </ul>
                     </li>
-                    
+                    <li><a><i class="fa fa-desktop"></i> Ajustes 3 <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="4.html">Botón 1</a></li>
+                            <li><a href="5.html">Botón 2</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="5.html">Tables</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-bar-chart-o"></i> Ajustes 4 <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="6.html">Chart JS</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="menu_section">
+                <h3>Aquí se puede colocar algún ecabezado</h3>
+                <ul class="nav side-menu">
+                    <li><a><i class="fa fa-bug"></i> Modificar esto <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="6.html">Ajustes5</a></li>
+                        </ul>
+                    </li>
+                    <li><a><i class="fa fa-windows"></i> Ajustes6 <span class="fa fa-chevron-down"></span></a>
+                        <ul class="nav child_menu">
+                            <li><a href="page_404.html">404 Error</a></li>
+                            <li><a href="page_500.html">500 Error</a></li>
+                            <li><a href="coming_soon.html">Coming Soon</a></li>
+                            <li><a href="plain_page.html">Plain Page</a></li>
+                            <li><a href="login.html">Login Page</a></li>
+                            <li><a href="sign_up.html">SignUp Page</a></li>
+ 
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+ 
+        </div>
+ 
+        <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Settings">
+                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Lock">
+                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+            </a>
+            <a data-toggle="tooltip" data-placement="top" title="Logout">
+                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+            </a>
+        </div>
+    </div>
+</div>
   </body>
 </html>
 
