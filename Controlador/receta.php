8b6646e35625d5c8cb58e2bf72729d8c7eb446e4
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-03-22 18:16:55 -0600

    reestructuracion de formulario dentro de receta.php

--- Controlador/receta.php
+++ Controlador/receta.php
@@ -1,37 +1,32 @@
 <?php
 
-/*
-#NO ESTOY seguro todavia como usar este codigo de objeto para las recetas, podria servir para heredar y subdividir la informacion especifica que tenemos sobre cada
-elemento de la receta sin embargo no se la logica ni la sintaxis para operar con esos datos
-de momento aca dejare nada mas a donde llegara la informacion del formulario pero no quiero borrar el objeto recetas tal vez puede servir en el futuro.
-
-
-class Receta
-    {
-        public $idReceta;
-        public $tituloPost;
-        public $descripcionPost;
-        public $imagenPost;
-        public $pasosPost;
-
-        function __construct($idReceta, $tituloPost, $descripcionPost, $imagenPost, $pasosPost)
-            {
-                $this->idReceta         = $idReceta;
-                $this->tituloPost       = $tituloPost;
-                $this->descripcionPost  = $descripcionPost;
-                $this->imagenPost       = $imagenPost;
-                $this->pasosPost        = $pasosPost;
-            }
-    }
-*/
 require "Controlador/config.php";
 
-
-$tituloPost = $_POST['tituloPost'];
-$descripcionPost = $_POST ['descripcionPost'];
-$pasosPost = $_POST['pasosPost'];
-$imagenPost = $_POST['imagenPost'];
-
-$sqlinsert = "INSERT INTO receta (tituloPost, descripcionPost, pasosPost, imagenPost) VALUES ($tituloPost, $descripcionPost, $pasosPost, $imagenPost)";
-
+        $tituloPost = $_POST['tituloPost'];
+        $descripcionPost = $_POST ['descripcionPost'];
+        $pasosPost = $_POST['pasosPost'];
+        $imagenPost = $_POST['imagenPost'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Nueva Receta.</title>
+</head>
+<body>
+
+<form name = "form" action = "receta.php" method = "post">
+
+    Titulo de la Receta.<br>            <input type = "text" name = "tituloPost" required>      <br><br>
+    Breve descripcion de la Receta.<br> <input type = "text" name = "descripcionPost" required> <br><br>
+    Paso a Paso.<br>                    <input type = "text" name = "pasosPost" required>       <br><br>
+                                        <input type = "submit" name = "Publicar.">              <br><br>
+    Subir imagen del platillo. <br>     <input type = "file" name = "imagenPost" required>
+
+
+</form>
+
+</body>
+</html>
