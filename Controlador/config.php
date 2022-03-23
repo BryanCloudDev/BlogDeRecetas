1607b138c30c528e3d311cd9a9aa196c0a4a631b
Bryan Portillo <bportillo701@gmail.com>
2022-03-23 09:02:19 -0600

    corrigienedo regada de contrasena de BD

--- Controlador/config.php
+++ Controlador/config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "1234";
+$password = "";
 
 try {
     $conn = new PDO("mysql:host=$servername;port=3306;dbname=blogreceta", $username, $password);
