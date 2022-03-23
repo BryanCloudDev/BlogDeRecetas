b3cd0d2af369639bd8750ae381b46978e3f4e442
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-03-23 11:50:59 -0600

    Reestructuracion MVC de las vistas

---
+++ Vista/receta.view.php
@@ -0,0 +1,5 @@
+<?php
+
+#html para mostrar en pantalla la receta
+
+?>
--- index.php
+++ index.php
@@ -1,28 +1,6 @@
-<?php require("Vista/resources/header.php");?>
+<?php
 
-<main class="main index">
-    <div class="container">
-        <?php for($i = 1;$i <= 5;$i++):?>
-        <!--Ta bonito el for para generar contenido a mostrar! -->
-        <article class="post">
-            <div class="imgContainer">
-                <img src="https://www.paulinacocina.net/wp-content/uploads/2020/01/untitled-copy.jpg" alt="img">
-            </div>
-            <div class="texto">
-                <h2><a href="./post?id=">Titulo de la receta</a></h2>
-                <p>Publicado el 3 de Diciembre de 2021</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum rem excepturi iste. Alias, autem delectus eligendi repudiandae explicabo rerum dolorum error exercitationem, quisquam corrupti adipisci ratione ipsa vero distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eum rem excepturi iste. Alias, autem delectus eligendi repudiandae explicabo rerum dolorum error exercitationem, quisquam corrupti adipisci ratione ipsa vero distinctio... <a href="#">Seguir leyendo</a></p>
-            </div>
-        </article>
-        <?php endfor;?>
-    </div>
-    <section class="paginacion">
-        <ul>
-            <li><a href="#">&lt;</a></li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">&gt;</a></li>
-        </ul>
-    </section>
-</main>
+#logica de index.
 
-<?php require("Vista/resources/footer.php") ?>
+
+?>
