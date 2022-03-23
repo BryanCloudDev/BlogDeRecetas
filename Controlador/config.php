02fc77ed9938a4bf8702f3f07ac2bf5fd0ae485e
unknown <enrique2016flores10@gmail.com>
2022-03-23 00:31:04 -0600

    se mejoro el post para las recetas, se creo la clase Receta

--- Controlador/config.php
+++ Controlador/config.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=blogreceta", $username, $password);
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=blogreceta", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
@@ -12,18 +12,69 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
-
-
-
-
-
-
-
 #FUNCION PARA INGRESAR VALORES A LA BD
 
-function insertarDatosBD($sqlinsert)
+class Receta{
+
+    public string $tituloReceta;
+    public string $descripcionReceta;
+    public string $pasosReceta;
+    public string $imagenReceta;
+    public object $db;
+    
+    public function __construct($tituloReceta,$descripcionReceta,$pasosReceta,$imagenReceta)
     {
-        $sqlinsert = "INSERT INTO receta (tituloPost, descripcionPost, pasosPost, imagenPost) VALUES ($tituloPost, $descripcionPost, $pasosPost, $imagenPost)";
+        global $conn;
+        $this->tituloReceta = $tituloReceta;
+        $this->descripcionReceta = $descripcionReceta;
+        $this->pasosReceta = $pasosReceta;
+        $this->imagenReceta = $imagenReceta;
+        $this->db = $conn;
     }
+
+
+    public function InsertReceta(){
+        $query = "INSERT INTO receta (tituloPost, descripcionPost, imagenPost, pasosPost) 
+                    VALUES (:tituloPost, :descripcionPost, :imagenPost,:pasosPost)";
+
+        $statement = $this->db->prepare($query);
+        $statement->bindValue(":tituloPost", $this->tituloReceta);
+        $statement->bindValue(":descripcionPost", $this->descripcionReceta);
+        $statement->bindValue(":imagenPost", $this->imagenReceta);
+        $statement->bindValue(":pasosPost", $this->pasosReceta);
+
+        $statement->execute();
+        
+    }
+
+    static function getRecetaById($id){
+        global $conn;
+        $query = "SELECT * FROM receta WHERE idReceta = :id";
+        $statement = $conn->prepare($query);
+        $statement->bindValue(":id", $id);
+        $statement->execute();
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    static function getAllRecetas(){
+        global $conn;
+        $query = "SELECT * FROM receta";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+
+function randomDIR($n){
+    $options = "abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    $result = "";
+    for($i = 0; $i < $n; $i++){
+        $random = rand(0,strlen($options) - 1);
+        $result .= $options[$random];
+    }
+
+    return $result;
+}
 
 ?>
--- Controlador/receta.php
+++ Controlador/receta.php
@@ -1,32 +1,58 @@
 <?php
 
-require "Controlador/config.php";
+require_once "./config.php";
 
-        $tituloPost = $_POST['tituloPost'];
-        $descripcionPost = $_POST ['descripcionPost'];
-        $pasosPost = $_POST['pasosPost'];
-        $imagenPost = $_POST['imagenPost'];
+if(isset($_POST["tituloPost"]) && isset($_POST["descripcionPost"]) && isset($_POST["pasosPost"]) && isset($_FILES["imagenPost"])){
+
+    ["tituloPost" => $tituloPost, 
+    "descripcionPost" => $descripcionPost, 
+    "pasosPost" => $pasosPost] = $_POST;
+
+    $imagenPost = $_FILES["imagenPost"] ?? null;
+
+    if(!is_dir(__DIR__ . "/images")){
+        mkdir(__DIR__ . "/images");
+    }
+
+    if($imagenPost && $imagenPost["tmp_name"]){
+        $imagenPostPath = "images/" . randomDIR(8) . "/" . $imagenPost["name"] ;
+        mkdir(dirname($imagenPostPath));
+        move_uploaded_file($imagenPost["tmp_name"],$imagenPostPath);
+    }
+
+    $receta = new Receta($tituloPost, $descripcionPost, $pasosPost, $imagenPostPath);
+    $receta->InsertReceta();
+
+}
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-
-    <title>Nueva Receta.</title>
+    <title>Nueva Receta</title>
 </head>
 <body>
+        <form name="form" action="receta.php" method="post" enctype="multipart/form-data">
 
-<form name = "form" action = "receta.php" method = "post">
+            <div class="form-group-2">
+                <label for="tituloPost">Titulo de la receta</label>
+                <input type="text" name="tituloPost" id="tituloPost" required>    
+            </div>
+            <div class="form-group-2">
+                <label for="descripcionPost">Breve descripcion de la receta</label>
+                <input type="text" name="descripcionPost" id="descripcionPost" require>
+            </div>
+            <div class="form-group-2">
+                <label for="pasosPost">Paso a Paso</label>
+                <textarea name="pasosPost" id="pasosPost" required></textarea>            
+            </div>
+            <div class="form-group-2">
+                <input type="file" name="imagenPost">
+            </div>
 
-    Titulo de la Receta.<br>            <input type = "text" name = "tituloPost" required>      <br><br>
-    Breve descripcion de la Receta.<br> <input type = "text" name = "descripcionPost" required> <br><br>
-    Paso a Paso.<br>                    <input type = "text" name = "pasosPost" required>       <br><br>
-                                        <input type = "submit" name = "Publicar.">              <br><br>
-    Subir imagen del platillo. <br>     <input type = "file" name = "imagenPost" required>
+                <input type="submit" name="Publicar">
 
-
-</form>
-
+        </form>
 </body>
 </html>
