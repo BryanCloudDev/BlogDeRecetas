5712fabbf320479c67544dc3bec94dbf7f15a1cb
Bryan Portillo <bportillo701@gmail.com>
2022-03-24 13:11:27 -0600

    Agregando estructira de perfil

--- Vista/perfil.view.php
+++ Vista/perfil.view.php
@@ -1,5 +1,21 @@
 <?php require("./Vista/componentes/header.php");?>
 
-<!-- Tu codigo HTML va aca-->
+<main class="main perfil">
+    <div class="container">
+        <section class="card">
+            <div class="header">
+                <div class="imgContainer">
+                    <img src="" alt="">
+                </div>
+                <div class="welcome">
+                    <h1></h1>
+                </div>
+            </div>
+            <div class="body">
+
+            </div>
+        </section>
+    </div>
+</main>
 
 <?php require("./Vista/componentes/footer.php"); ?>
