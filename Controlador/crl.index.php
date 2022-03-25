3bf6d12f07d0c6018b35f90d91ddb652878b035d
unknown <enrique2016flores10@gmail.com>
2022-03-25 16:46:00 -0600

    se automatizo la llamada de las recetas

--- Controlador/crl.index.php
+++ Controlador/crl.index.php
@@ -5,6 +5,13 @@ session_start();
 require_once "Controlador/crl.config.php";
 require_once "Modelos/funciones.receta.php";
 
-$recetas = Receta::getAllRecetas();
+if(isset($_GET["id"])){
+    $recetas = [Receta::getRecetaById($_GET["id"])];
+}else if(isset($_POST["search"])){
+    $recetas = Receta::getRecetaByTitle($_POST["search"]);
+}else{
+    $recetas = Receta::getAllRecetas();
+}
+
 
 ?>
