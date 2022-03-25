09ec3f177c4220a00b8ca3fcee0d034a33eaeeb3
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-03-25 09:01:04 -0600

    Comenzando el controlador de la pagina de respuestas

--- Controlador/crl.resultados.php
+++ Controlador/crl.resultados.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+require_once "crl.config.php";
+require_once "../Modelos/funciones.receta.php";
+
+$resultadoGlobal = Receta::getAllRecetas();
 #logica para traer resultados de busqueda de recetas
 
 ?>
