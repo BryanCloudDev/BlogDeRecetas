029ae0ebfe3d68f2514640402780885d19e87801
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-03-25 19:33:29 -0600

    empezando la vista de las recetas

--- Vista/resultados.view.php
+++ Vista/resultados.view.php
@@ -2,4 +2,20 @@
 
 <!-- Tu codigo HTML va aca-->
 
+
+<!--aqui la variable receta se manda a un foreach para que se pueda mostrar en pantalla dato por dato -->
+<?php foreach($recetas as $receta): ?>
+    <div>
+        <div>
+            <img src="<?php echo $receta["imagenPost"]?>">
+        </div>
+        <div>
+        <h1><?php echo $receta['tituloPost']?></h1>
+        <span>Este post fue creado el <?php echo $receta["fecha"]?> </span>
+        </div>
+        <p> <?php echo $receta["pasosPost"]?> </p>
+        </div>
+    </div>
+<?php endforeach; ?>
+
 <?php require("./Vista/componentes/footer.php"); ?>
