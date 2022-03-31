7ba0ec4f1820a888950f64ec616abb89228d6e3b
Bryan Portillo <bportillo701@gmail.com>
2022-03-31 12:54:51 -0600

    Arreglando perfil

--- Vista/perfil.view.php
+++ Vista/perfil.view.php
@@ -29,33 +29,20 @@
             <h1>Posts creados por <?php //nombre de usuario?></h1>
             <span>Se han creado <?php echo count($recetas) ?> <?php echo count($recetas) === 1 ? "post" : "posts" ?></span>
         </section>
-        <!-- <section class="post">
-            <div class="imgContainer">
-                <img src="<?php #$receta["imagenPost"] ?>" alt="Comida o algo">
-            </div>
-            <div class="texto">
-                <h2><a href="./post?id="><?php #$receta["tituloPost"] ?></a></h2>
-                <p>Publicado el 3 de Diciembre de 2021 </p>
-                <p><?php #$receta["pasosPost"] ?></p>
-            </div>
-        </section> -->
-        <!-- todos los posts hehcos por el usuario -->
-        
         <?php foreach($recetas as $receta): ?>
             <article class="post">
                 <div class="imgContainer">
                     <img src="<?= $receta["imagenPost"] ?>" alt="Comida o algo">
                 </div>
                 <div class="texto">
-                    <h2><a href="index.php?id=<?= $receta["idReceta"] ?>"><?= $receta["tituloPost"] ?></a></h2>
-                    <p>Este post fue creado el <?= $receta["fecha"] ?? "Miercoles 16 de Marzo de 2022" ?></p>
-                    <p>Descripción : <?= $receta["descripcionPost"] ?></p>
-                    <p><?= $receta["pasosPost"] ?></p>
+                    <h2><a href="receta.php?id=<?= $receta["idReceta"] ?>"><?= $receta["tituloPost"] ?></a></h2>
+                    <p class="date">Creado el <?= $receta["fecha"] ?? "Miercoles 16 de Marzo de 2022" ?></p>
+                    <p><?= $receta["descripcionPost"] ?></p>
                 </div>
             </article>
         <?php endforeach ?>
+            <a class="newPost" href="registrar_receta.php">Crear Post <i class="fa fa-plus" aria-hidden="true"></i></a>
         </article>
     </div>
 </main>
-
 <?php require("./Vista/componentes/footer.php"); ?>
