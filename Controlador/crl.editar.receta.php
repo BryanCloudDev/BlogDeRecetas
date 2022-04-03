0a91c37e0d1d9ae73e41d0b4e6bce4b6b3501a41
unknown <enrique2016flores10@gmail.com>
2022-04-03 17:20:57 -0600

    se ha eliminado este archivo

--- Controlador/crl.editar.receta.php
+++
@@ -1,7 +0,0 @@
-<?php 
- 
- require_once "./Controlador/crl.isuser.php";
- require_once "./Controlador/crl.config.php";
- require_once "./Modelos/funciones.spanishdate.php";
- require_once "./Modelos/funciones.receta.php";
-
