55349c9de7b82b6ccdffba7e5de8e516a274f5bf
Bryan Portillo <bportillo701@gmail.com>
2022-04-03 12:45:26 -0600

    Arreglando vista de receta

--- Vista/receta.view.php
+++ Vista/receta.view.php
@@ -1,17 +1,19 @@
 <?php require("./Vista/componentes/header.php"); ?>
 
-<!-- Tu codigo HTML va aca-->
-<a href="index.php">Ir al menú principal</a>
-<article class="post">
-    <div class="imgContainer">
-        <img src="<?= $receta["imagenPost"] ?>" alt="Comida o algo">
+<main class="main index">
+    <!-- contenedor para centrar el sitio web -->
+    <div class="container">
+        <article class="post">
+            <div class="imgContainer">
+                <img src="<?= $receta["imagenPost"] ?>" alt="Comida o algo">
+            </div>
+            <div class="texto">
+                <h2><a href="receta.php?id=<?= $receta["idReceta"] ?>"><?= $receta["tituloPost"] ?></a></h2>
+                <p class="date">Creado el <?= $receta["fecha"] ?? "Miercoles 16 de Marzo de 2022" ?></p>
+                <p><?= $receta["pasosPost"] ?></p>
+            </div>
+        </article>
     </div>
-    <div class="texto">
-        <h2><?= $receta["tituloPost"] ?></h2>
-        <p>Este post fue creado el <?= $receta["fecha"] ?? "Miercoles 16 de Marzo de 2022" ?></p>
-        <p>Descripción breve: <?= $receta["descripcionPost"] ?></p>
-        <p>pasos generales de la receta : <?= $receta["pasosPost"] ?></p>
-    </div>
-</article>
+</main>
 
 <?php require("./Vista/componentes/footer.php"); ?>
