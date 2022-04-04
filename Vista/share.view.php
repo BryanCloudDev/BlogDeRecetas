3ff4a6c7ee4573a0440b8518c1daaa0aaae370f4
Arce <arcetechnologies@gmail.com>
2022-04-04 12:03:19 -0600

    arreglando los iconos

--- Vista/share.view.php
+++ Vista/share.view.php
@@ -15,22 +15,12 @@
  
     <!-- Facebook Social Media -->
     <a href="http://www.facebook.com/sharer.php?u=<?=$site_url?>" target="_blank">
-        <img src="https://cdn-icons.flaticon.com/png/512/3128/premium/3128304.png" alt="Facebook share link" />
-    </a>
-    
-    <!-- LinkedIn Social Media -->
-    <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?=$site_url?>" target="_blank">
-        <img src="http://www.onlinecode.org/example/images/linkedin.png" alt="LinkedIn share link" />
-    </a>
-    
-    <!-- Reddit Social Media -->
-    <a href="http://reddit.com/submit?url=<?=$site_url?>&amp;title=<?=$site_title?>" target="_blank">
-        <img src="http://www.onlinecode.org/example/images/reddit.png" alt="Reddit share link" />
+        <img src="https://spng.pngfind.com/pngs/s/439-4392840_facebook-link-icon-image-dynamic-spectrum-alliance-pink.png" alt="Facebook share link" />
     </a>
      
     <!-- Twitter Social Media -->
     <a href="https://twitter.com/share?url=<?=$site_url?>&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
-        <img src="http://www.onlinecode.org/example/images/twitter.png" alt="Twitter share link" />
+        <img src="https://img.icons8.com/color/344/twitter--v1.png" alt="Twitter share link" />
     </a>
 
 </div>
