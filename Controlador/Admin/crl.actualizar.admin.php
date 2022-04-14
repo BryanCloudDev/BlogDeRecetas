06f6299b7cf19703099d62284f84fca7b992ede9
KevinArce <iav.kevinarce@ufg.edu.sv>
2022-04-14 17:06:29 -0600

    Viendo que peroxido de hidrogeno con este Git xd

--- Controlador/Admin/crl.actualizar.admin.php
+++ Controlador/Admin/crl.actualizar.admin.php
@@ -6,6 +6,6 @@ require_once ('Modelos/funciones.referer.php');
 require_once ('Modelos/funciones.user.php');
 
 #Vamo a hacer todo de welta :v
-#Solo viendo que pedo bandita
+#Solo viendo que pedo bandita 2.0
 
 ?>
