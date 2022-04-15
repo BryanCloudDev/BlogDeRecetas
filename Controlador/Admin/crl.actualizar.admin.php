25d71d9a3ec69ee14677e6df7c36ef7261b675e7
KevinArce <iav.kevinarce@ufg.edu.sv>
2022-04-14 18:23:01 -0600

    Hoy si espero que funcione este pedo xdd

--- Controlador/Admin/crl.actualizar.admin.php
+++ Controlador/Admin/crl.actualizar.admin.php
@@ -6,6 +6,6 @@ require_once ('Modelos/funciones.referer.php');
 require_once ('Modelos/funciones.user.php');
 
 #Vamo a hacer todo de welta :v
-#Solo viendo que pedo bandita 3.0
+#Solo viendo que pedo bandita 4.0
 
 ?>
