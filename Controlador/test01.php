fdc4f0088eb90e5b11bc7a9a085c1921684a495e
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-04-21 10:44:33 -0600

    aprendiendo a usar unit en el proyecto

---
+++ Controlador/test01.php
@@ -0,0 +1,13 @@
+<?php
+
+class Operations
+    {
+        public function sum($n1,$n2)
+            {
+                return $n1 + $n2;
+            }
+        public  function div($n1,$n2)
+            {
+                return $n1 / $n2;
+            }
+    }
